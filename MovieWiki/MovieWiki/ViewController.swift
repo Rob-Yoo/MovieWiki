59ed688bd9910e7be911de1f15b8afbc3b7ba0a4
Rob-Yoo <jin12243@naver.com>
2024-06-10 21:38:42 +0900

    프로젝트 세팅 완료

---
+++ MovieWiki/MovieWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieWiki
+//
+//  Created by Jinyoung Yoo on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
